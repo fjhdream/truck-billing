01d92786388867617f97b1e2d97788a81cda10a8
fjhdream <fjh_dream@163.com>
2022-09-11 00:11:31 +0800

    post user add to db

--- src/user_service/controller.rs
+++ src/user_service/controller.rs
@@ -1,7 +1,7 @@
-use poem_openapi::{
-    payload::{Json, PlainText},
-    ApiResponse, Object, OpenApi, Tags,
-};
+use crate::{entities::user, DATABASE};
+use poem_openapi::{payload::Json, ApiResponse, Object, OpenApi, Tags};
+use sea_orm::{ActiveModelTrait, Set};
+use tracing::log::warn;
 
 #[derive(Tags)]
 enum ApiTags {
@@ -26,6 +26,9 @@ struct UserDTO {
 enum CreateUserResponse {
     #[oai(status = 200)]
     Ok(Json<String>),
+
+    #[oai(status = 500)]
+    Error,
 }
 
 pub struct UserRouter;
@@ -34,6 +37,18 @@ pub struct UserRouter;
 impl UserRouter {
     #[oai(path = "/", method = "post", tag = "ApiTags::User")]
     async fn create(&self, user: Json<UserDTO>) -> CreateUserResponse {
+        let db = DATABASE.get().unwrap();
+        let insert_result = user::ActiveModel {
+            id: Set(user.id.to_owned()),
+            user_name: Set(user.name.to_owned()),
+            avatar_url: Set(user.avatar_url.to_owned()),
+        }
+        .insert(db)
+        .await;
+        if let Err(err) = insert_result {
+            warn!("insert user met error {}", err);
+            return CreateUserResponse::Error;
+        }
         return CreateUserResponse::Ok(Json(user.0.id));
     }
 }
