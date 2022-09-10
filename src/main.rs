58628b8ab354205ae2a549b27d7c3bda76d1c830
fjhdream <fjh_dream@163.com>
2022-09-10 19:57:37 +0800

    add sea-orm

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,31 @@
 // main.rs
+mod entities;
 
-use sea_orm::{Database, DbErr};
-use poem::{get, handler, listener::TcpListener, web::Path, IntoResponse, Route, Server};
+use poem::{get, handler, listener::TcpListener, web::Path, Route, Server};
+use entities::{prelude::*, *};
+use sea_orm::*;
 
 //postgreSql
 const DATABASE_URL: &str = "postgres://postgres:01010727.@localhost:5432/truck-db";
 
 #[handler]
 async fn hello(Path(name): Path<String>) -> String {
-    if let Err(err) = run().await {
-        panic!("{}", err);
-    }
-    format!("hello: {}, db is connected!", name)
-}
+    if let Ok(db) = Database::connect(DATABASE_URL).await{
 
-async fn run() -> Result<(), DbErr> {
-    let db = Database::connect(DATABASE_URL).await?;
-    Ok(())
+        format!("hello: {}, db is connected!", name)
+    } else {
+        return format!("connect db error");
+    }
+    
+
 }
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .with_test_writer()
+        .init();
     let app = Route::new().at("/hello/:name", get(hello));
     Server::new(TcpListener::bind("127.0.0.1:3000"))
         .run(app)
