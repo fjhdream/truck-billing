a6dfdc7ca5c4bebcff9afd549b672a098d971547
fjhdream <fjh_dream@163.com>
2022-09-10 20:16:52 +0800

    connect local db

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@
 mod entities;
 
 use poem::{get, handler, listener::TcpListener, web::Path, Route, Server};
-use entities::{prelude::*, *};
+use entities::*;
 use sea_orm::*;
+use entities::user::Entity as UserEntity;
 
 //postgreSql
 const DATABASE_URL: &str = "postgres://postgres:01010727.@localhost:5432/truck-db";
@@ -11,13 +12,15 @@ const DATABASE_URL: &str = "postgres://postgres:01010727.@localhost:5432/truck-d
 #[handler]
 async fn hello(Path(name): Path<String>) -> String {
     if let Ok(db) = Database::connect(DATABASE_URL).await{
-
-        format!("hello: {}, db is connected!", name)
+        let user_query :Result<Option<user::Model>, DbErr> = UserEntity::find_by_id("1232".to_string()).one(&db).await;
+        if let Ok(Some(user)) = user_query {
+            return format!("hello: {}, db is connected!", user.user_name);
+        } else {
+            return format!("get empty user, hello: {}", name);
+        }  
     } else {
         return format!("connect db error");
     }
-    
-
 }
 
 #[tokio::main]
