4ade89ea4c64e1075e03cd7f4617cf032832f0a7
fjhdream <fjh_dream@163.com>
2022-09-10 20:36:50 +0800

    add poem-api

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,23 @@ mod entities;
 
 use poem::{get, handler, listener::TcpListener, web::Path, Route, Server};
 use entities::*;
+use poem_openapi::{OpenApi, payload::PlainText, OpenApiService};
 use sea_orm::*;
 use entities::user::Entity as UserEntity;
 
 //postgreSql
 const DATABASE_URL: &str = "postgres://postgres:01010727.@localhost:5432/truck-db";
+
+struct Api;
+
+#[OpenApi]
+impl Api {
+    /// Hello world
+    #[oai(path = "/", method = "get")]
+    async fn index(&self) -> PlainText<&'static str> {
+        PlainText("Hello World")
+    }
+}
 
 #[handler]
 async fn hello(Path(name): Path<String>) -> String {
@@ -29,7 +41,11 @@ async fn main() -> Result<(), std::io::Error> {
         .with_max_level(tracing::Level::DEBUG)
         .with_test_writer()
         .init();
-    let app = Route::new().at("/hello/:name", get(hello));
+    let api_service =
+    OpenApiService::new(Api, "Hello World", "1.0").server("http://localhost:3000");
+    let ui = api_service.swagger_ui();
+    let api_service = Route::new().at("/hello/:name", get(hello));
+    let app = Route::new().nest("/", api_service).nest("/docs", ui);
     Server::new(TcpListener::bind("127.0.0.1:3000"))
         .run(app)
         .await
