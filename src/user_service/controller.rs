e0dde146e4f8791a47d8e3f3ea3f50f1cb497c9a
fjhdream <fjh_dream@163.com>
2022-09-22 20:24:37 +0800

    get user API

--- src/user_service/controller.rs
+++ src/user_service/controller.rs
@@ -1,5 +1,6 @@
+use poem::web::Path;
 use poem_openapi::{payload::Json, ApiResponse, Object, OpenApi, Tags};
-use tracing::log::warn;
+use tracing::{log::error, log::warn};
 
 use super::service::UserAggregate;
 
@@ -24,8 +25,28 @@ pub struct UserDTO {
 
 #[derive(ApiResponse)]
 enum CreateUserResponse {
-    #[oai(status = 200)]
+    #[oai(status = 201)]
     Ok(Json<String>),
+
+    #[oai(status = 500)]
+    Error,
+}
+
+#[derive(Debug, Object, Clone, Eq, PartialEq)]
+pub struct UserEntity {
+    pub id: String,
+
+    pub name: String,
+
+    pub avatar_url: Option<String>,
+
+    pub role: Vec<String>,
+}
+
+#[derive(ApiResponse)]
+enum GetUserResponse {
+    #[oai(status = 200)]
+    Ok(Json<UserEntity>),
 
     #[oai(status = 500)]
     Error,
@@ -45,5 +66,33 @@ impl UserRouter {
             return CreateUserResponse::Error;
         }
         CreateUserResponse::Ok(Json(create_result.unwrap()))
+    }
+
+    #[oai(path = "/user/:user_id", method = "get", tag = "ApiTags::User")]
+    async fn get(&self, user_id: Path<String>) -> GetUserResponse {
+        let user_id = user_id.0;
+        let user_aggregate_result = UserAggregate::from_user_id(user_id.clone()).await;
+        if let Err(err) = user_aggregate_result {
+            error!(
+                "Get user form db error {}! user is is {}",
+                err,
+                user_id.clone()
+            );
+            return GetUserResponse::Error;
+        }
+        let user_aggregate = user_aggregate_result.unwrap();
+        let query_role_result = user_aggregate.get_user_role().await;
+        if let Err(err) = query_role_result {
+            warn!("create user met error {}", err);
+            return GetUserResponse::Error;
+        }
+        let query_role = query_role_result.unwrap();
+        let user_entity = UserEntity {
+            id: user_aggregate.id,
+            name: user_aggregate.name,
+            avatar_url: user_aggregate.avatar_url,
+            role: query_role.roles,
+        };
+        GetUserResponse::Ok(Json(user_entity))
     }
 }
--- src/user_service/service.rs
+++ src/user_service/service.rs
@@ -1,20 +1,78 @@
-use sea_orm::{ActiveModelTrait, DbErr, Set};
+use sea_orm::{ActiveModelTrait, ColumnTrait, DbErr, EntityTrait, QueryFilter, Set};
+use std::{error::Error, vec};
 use tracing::instrument;
 
-use crate::{entities::user, role_service::service::UserRoleAggregate, DATABASE};
+use crate::{
+    entities::{role, sea_orm_active_enums::RoleType, user},
+    role_service::service::UserRoleAggregate,
+    DATABASE,
+};
 
 use super::controller::UserDTO;
 
 #[derive(Debug)]
+pub enum UserError {
+    EmptyUserError,
+    DbError,
+}
+
+impl Error for UserError {}
+
+impl std::fmt::Display for UserError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            UserError::EmptyUserError => {
+                write!(f, "There is nothing with this man.")
+            }
+            UserError::DbError => {
+                write!(f, "Connect with Db Error")
+            }
+        }
+    }
+}
+
+impl From<DbErr> for UserError {
+    fn from(_: DbErr) -> Self {
+        UserError::DbError
+    }
+}
+
+impl ToString for RoleType {
+    fn to_string(&self) -> String {
+        match self {
+            RoleType::Admin => "ADMIN".to_owned(),
+            RoleType::Driver => "DRIVER".to_owned(),
+            RoleType::Owner => "OWNER".to_owned(),
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct UserAggregate {
-    id: String,
-    name: String,
-    avatar_url: Option<String>,
+    pub id: String,
+    pub name: String,
+    pub avatar_url: Option<String>,
 }
 
 impl UserAggregate {
     #[instrument]
-    pub async fn create_user(self) -> Result<String, DbErr> {
+    pub async fn from_user_id(user_id: String) -> Result<UserAggregate, UserError> {
+        let db = DATABASE.get().unwrap();
+        let query_result = user::Entity::find_by_id(user_id).one(db).await?;
+        if query_result.is_none() {
+            return Err(UserError::EmptyUserError);
+        }
+        let query_model = query_result.unwrap();
+        let user = UserAggregate {
+            id: query_model.id,
+            name: query_model.user_name,
+            avatar_url: query_model.avatar_url,
+        };
+        return Ok(user);
+    }
+
+    #[instrument]
+    pub async fn create_user(self) -> Result<String, UserError> {
         let db = DATABASE.get().unwrap();
         user::ActiveModel {
             id: Set(self.id.to_owned()),
@@ -28,6 +86,20 @@ impl UserAggregate {
         role.save().await?;
         Ok(self.id.clone())
     }
+
+    #[instrument]
+    pub async fn get_user_role(&self) -> Result<UserAggregateRole, UserError> {
+        let db = DATABASE.get().unwrap();
+        let query_result = role::Entity::find()
+            .filter(role::Column::UserId.eq(self.id.clone()))
+            .all(db)
+            .await?;
+        let mut role_array: Vec<String> = vec![];
+        for query_model in query_result {
+            role_array.push(query_model.r#type.to_string());
+        }
+        Ok(UserAggregateRole { roles: role_array })
+    }
 }
 
 impl From<UserDTO> for UserAggregate {
@@ -38,4 +110,8 @@ impl From<UserDTO> for UserAggregate {
             avatar_url: user.avatar_url,
         }
     }
+}
+
+pub struct UserAggregateRole {
+    pub roles: Vec<String>,
 }
